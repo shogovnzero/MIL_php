50d1e1e605b6b2a7a5d870186de443bd2a9d4f6d
shogovnzero <shogovnzero@gmail.com>
2022-06-13 04:37:27 +0900

    20220613-2

--- 01-php_okamoto_08/read.php
+++ 01-php_okamoto_08/read.php
@@ -9,6 +9,15 @@ $file = "data/data.csv";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Review - View All</title>
+    <style>
+      table {
+        border-collapse: collapse;
+      }
+      td {
+        border: solid 1px black;
+        padding: 3px;
+      }
+    </style>
 </head>
 <body>
     <div class="container">
